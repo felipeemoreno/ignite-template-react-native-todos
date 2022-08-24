3045b978e61579dc854f81d9dc47ce8c392a09de
tbdev <tbdev@debian>
2022-08-23 22:53:13 -0300

    done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,19 +5,37 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+
+    const newTask:Task = Object.assign({}, {
+      title: newTaskTitle,
+      id: new Date().getTime(),
+      done: false,
+    })
+    setTasks((prevState) => [...prevState, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const todosChanged = tasks.map(item => {
+      if(item.id === id) {
+        item.done = !item.done
+      }
+      return item;
+    });
+
+    setTasks(todosChanged)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const tasksFiltered = tasks.filter(task => task.id !== id)
+
+    console.log(tasksFiltered)
+
+    setTasks(tasksFiltered)
   }
 
   return (
